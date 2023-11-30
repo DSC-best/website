454eac9c24f1f0c67418ab8003d40938b1ec3143
Koffe <78866073+ThatKoffe@users.noreply.github.com>
2023-11-30 23:34:07 +0100

    funky prisma fix

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,12 @@
 import prisma from '$lib/server/prisma';
 import SafeBot from '$lib/structures/bot';
-// import { BotApprovalStatus } from '@prisma/client';
+import { BotApprovalStatus } from '@prisma/client';
 import botTags from '$lib/server/botTags';
 
 export async function load({}) {
 	const newBots = await prisma.bot.findMany({
 		where: {
-			// approval_status: BotApprovalStatus.APPROVED
+			approval_status: BotApprovalStatus.APPROVED
 		},
 		orderBy: {
 			approved_time: 'desc'
