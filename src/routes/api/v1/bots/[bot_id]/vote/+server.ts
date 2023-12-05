f040c80ed94ee82b64e95c9f40d554016e063644
Koffe <78866073+ThatKoffe@users.noreply.github.com>
2023-12-05 19:44:22 +0100

    Add log when user votes for bot

--- src/routes/api/v1/bots/[bot_id]/vote/+server.ts
+++ src/routes/api/v1/bots/[bot_id]/vote/+server.ts
@@ -3,6 +3,12 @@ import { json } from '@sveltejs/kit';
 import { _voteTimeout } from '../../../../../bots/[bot_id]/vote/+page.server';
 import { BotApprovalStatus } from '@prisma/client';
 import snowflake from '$lib/server/snowflake';
+import ChannelLog from '$lib/server/discord/channelLog';
+import { APP_URL } from '$env/static/private';
+import SafeBot from '$lib/structures/bot';
+import { InternalColors } from '$lib/server/colors';
+
+const channelLog = new ChannelLog();
 
 export async function POST({ locals, params }) {
 	const bot = await prisma.bot.findUnique({
@@ -55,6 +61,16 @@ export async function POST({ locals, params }) {
 			}
 		}
 	});
+
+	const safeBot = SafeBot(bot);
+
+	await channelLog.sendLog(
+		`Bot voted for`,
+		`Bot [${safeBot?.username}](${APP_URL}/bots/${bot?.id}) was voted for by <@!${locals?.actor
+			?.id}>, now at ${bot?.vote_count + 1} votes`,
+		null,
+		InternalColors.Green
+	);
 
 	return json({ message: 'OK' });
 }
